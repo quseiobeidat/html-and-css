c0322a6e8fba18bb519fcf5968c2fd5d10251578
quseiobeidat <quseialobeidat@gmail.com>
2021-02-23 15:54:35 +0200

    Update

---
+++ Javascript_project/js/js.js
@@ -0,0 +1,23 @@
+alert ("Hello.") ; 
+var age = prompt ("What is your age");
+if(age<18){
+//console.log("you are below the legal age !");
+alert("you are below the legal age!");
+console.log("test1");
+
+}
+
+if(age>=19){
+  alert("wlcome friend");
+console.log("test2");
+
+}
+var userName=''
+var player1=prompt("player one name");
+var player2=prompt("player two name");
+
+if (player1===player2){
+  console.log("simil")
+
+}
+
