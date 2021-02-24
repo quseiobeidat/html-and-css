3aa8efd3136b5fc1f50fa6e8d34fe8fd3c288758
qusei obeidat <79080983+quseiobeidat@users.noreply.github.com>
2021-02-24 20:16:13 +0200

    Update js.js

--- lab_5_6_project/js/js.js
+++ lab_5_6_project/js/js.js
@@ -1,12 +1,13 @@
 
-function myfunction () {
-    for (i = 0; i < 20; i++) {
-        var x = prompt ("put 20");
-        if (x == 20) {
-            break; }}
+alert ("hello")
+// function myfunction () {
+//     for (i = 0; i < 20; i++) {
+//         var x = prompt ("put 20");
+//         if (x == 20) {
+//             break; }}
     
-      var y = prompt ("put a random number "); 
-      for (i = 0; i < y; i++) {
-        document.write('<img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg">');}
+//       var y = prompt ("put a random number "); 
+//       for (i = 0; i < y; i++) {
+//         document.write('<img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg">');}
 
-myfunction ()
+// myfunction ()
