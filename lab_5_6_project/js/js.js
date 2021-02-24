962e3ebf01cd498b6699c69a843cd68dca67dd37
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-02-24 20:22:19 +0200

    ccc

--- lab_5_6_project/js/js.js
+++ lab_5_6_project/js/js.js
@@ -1,13 +1,13 @@
 
-alert ("hello") ;
-// function myfunction () {
-//     for (i = 0; i < 20; i++) {
-//         var x = prompt ("put 20");
-//         if (x == 20) {
-//             break; }}
-    
-//       var y = prompt ("put a random number "); 
-//       for (i = 0; i < y; i++) {
-//         document.write('<img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg">');}
 
-// myfunction ()
+function myfunction () {
+    for (i = 0; i < 20; i++) {
+        var x = prompt ("put 20");
+        if (x == 20) {
+            break; }}
+    
+      var y = prompt ("put a random number "); 
+      for (i = 0; i < y; i++) {
+        document.write('<img src="https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2019/07/Man-Silhouette.jpg">');}}
+
+myfunction()
