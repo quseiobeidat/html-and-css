6c63735608cb7c1f47ba32360cbdec08754085ff
qusei obeidat <79080983+quseiobeidat@users.noreply.github.com>
2021-02-25 09:49:33 +0200

    Create js.js

---
+++ For_JS_project/js/js.js
@@ -0,0 +1,21 @@
+
+alert("Hello!, click on Ok :) ");
+alert("Keep your eyes on the console ");
+var x = prompt("What is your name ");
+alert("Hello" + " " + x);
+console.log ("Hello" + " " + x);
+
+var y = prompt("What is the result of 5+5? ");
+
+if (y==10) {
+
+    alert("Corrrrrect!!! ");
+
+}
+
+else {
+
+alert("Wrong!!! ");
+
+} 
+
